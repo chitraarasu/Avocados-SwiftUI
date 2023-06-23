0e4978f8b7fdab17b20735c85b2d63d551b09e89
Chitraarasu <98887175+chitraarasu-kirshi@users.noreply.github.com>
2023-06-23 11:23:25 +0530

    Initial Commit

---
+++ Avocados/AvocadosApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvocadosApp.swift
+//  Avocados
+//
+//  Created by kirshi on 6/23/23.
+//
+
+import SwiftUI
+
+@main
+struct AvocadosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
